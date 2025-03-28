772573e6b8a1af63fbaa1ad6b5c8f87a0cee0198
Esteban-Ospina93 <estebanospinasalcedo@gmail.com>
2025-03-28 12:27:44 -0500

    Inicio del proyecto, preparando el entorno

---
+++ src/main/java/com/cesde/inventario/gestor_de_herramientas/GestorDeHerramientasApplication.java
@@ -0,0 +1,13 @@
+package com.cesde.inventario.gestor_de_herramientas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeHerramientasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeHerramientasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesde/inventario/gestor_de_herramientas/GestorDeHerramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.inventario.gestor_de_herramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeHerramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
